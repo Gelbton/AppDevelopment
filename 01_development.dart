64044766004f489f9baebbe26ffaa539f572f708
Gelbton <59143100+Gelbton@users.noreply.github.com>
2020-07-13 14:44:45 +0200

    Create 01_development.dart

---
+++ 01_development.dart
@@ -0,0 +1,14 @@
+void main() {
+ int zahl = 0;
+ int zahl1 = 1;
+  print(zahl);
+  print(zahl1);
+  
+ double komma = 0.1;
+   double komma1= 0.2;
+  print(komma);
+  print(komma1);
+  
+  String satz = "Hallo Welt!";
+  print (satz);
+}
