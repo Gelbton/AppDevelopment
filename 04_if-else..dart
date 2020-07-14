b90de5ffc185eadfc5a34202e71323ff3d5dd3c0
Gelbton <59143100+Gelbton@users.noreply.github.com>
2020-07-14 11:45:19 +0200

    Create 04_if-else..dart

---
+++ 04_if-else..dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  int zahlengrenze = 8;
+  
+  if(zahlengrenze > 10)
+  {
+    print ("Größer als 10");
+  }
+  else
+    print("Kleiner gleich  10");
+  
+  String output = zahlengrenze > 10 ? "größer als 10" : "kleiner gleich 10";
+  print (output);
+}
